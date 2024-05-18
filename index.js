8adac89baeb8f8ade16b1719eaf04aa94fb47f4f
sharoons10 <94461677+sharoons10@users.noreply.github.com>
2024-05-18 21:29:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,74 +1,65 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
-const port = process.env.PORT || 5000;
-
-const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
-const jwt = require('jsonwebtoken');
-
-// middleware
-app.use(cors());
-app.use(express.json());
-
-mongoose
-  .connect(
-    'mongodb+srv://sharoonsunkari12:yp9aGIdBvGJpKouo@complete-foodi-client.scpedx7.mongodb.net/?retryWrites=true&w=majority&appName=complete-foodi-client')
-  .then(console.log("Mongodb connected successfully!"))
-  .catch((error) => console.log("Error connecting to MongoDB: " + error));
-
-// jwt authentication
-
-// jwt related api
-app.post("/jwt", async (req, res) => {
-  const user = req.body;
-  // console.log(user)
-  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "1h",
-  });
-  res.send({ token });
-});
-
-// import routes
-const menuRoutes = require("./api/routes/menuRoutes");
-const cartsRoutes = require("./api/routes/cartRoutes");
-const usersRoutes = require("./api/routes/userRoutes");
-const paymentRoutes = require("./api/routes/paymentRoutes");
-const adminStats =  require('./api/routes/adminStats');
- const orderStats = require('./api/routes/orderStats')
-app.use("/menu", menuRoutes);
-app.use("/carts", cartsRoutes);
-app.use("/users", usersRoutes);
-app.use("/payments", paymentRoutes);
-app.use('/admin-stats', adminStats);
-app.use('/order-stats', orderStats);
-
-
-// payment methods routes
-const verifyToken = require('./api/middlewares/verifyToken')
-
-app.post("/create-payment-intent",verifyToken, async (req, res) => {
-  const { price } = req.body;
-  const amount = parseInt(price*100);
-  // console.log(amount);
-
-  // Create a PaymentIntent 
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: amount,
-    currency: "usd",
-    payment_method_types: ["card"],
-  });
-
-  res.send({
-    clientSecret: paymentIntent.client_secret,
-  });
-});
-
-app.get("/", (req, res) => {
-  res.send("Foodi Server is Running!");
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+"use strict";
+const getCodePoint = (character) => character.codePointAt(0);
+const first = (x) => x[0];
+const last = (x) => x[x.length - 1];
+function toCodePoints(input) {
+    const codepoints = [];
+    const size = input.length;
+    for (let i = 0; i < size; i += 1) {
+        const before = input.charCodeAt(i);
+        if (before >= 0xd800 && before <= 0xdbff && size > i + 1) {
+            const next = input.charCodeAt(i + 1);
+            if (next >= 0xdc00 && next <= 0xdfff) {
+                codepoints.push((before - 0xd800) * 0x400 + next - 0xdc00 + 0x10000);
+                i += 1;
+                continue;
+            }
+        }
+        codepoints.push(before);
+    }
+    return codepoints;
+}
+function saslprep({ unassigned_code_points, commonly_mapped_to_nothing, non_ASCII_space_characters, prohibited_characters, bidirectional_r_al, bidirectional_l, }, input, opts = {}) {
+    const mapping2space = non_ASCII_space_characters;
+    const mapping2nothing = commonly_mapped_to_nothing;
+    if (typeof input !== 'string') {
+        throw new TypeError('Expected string.');
+    }
+    if (input.length === 0) {
+        return '';
+    }
+    const mapped_input = toCodePoints(input)
+        .map((character) => (mapping2space.get(character) ? 0x20 : character))
+        .filter((character) => !mapping2nothing.get(character));
+    const normalized_input = String.fromCodePoint
+        .apply(null, mapped_input)
+        .normalize('NFKC');
+    const normalized_map = toCodePoints(normalized_input);
+    const hasProhibited = normalized_map.some((character) => prohibited_characters.get(character));
+    if (hasProhibited) {
+        throw new Error('Prohibited character, see https://tools.ietf.org/html/rfc4013#section-2.3');
+    }
+    if (opts.allowUnassigned !== true) {
+        const hasUnassigned = normalized_map.some((character) => unassigned_code_points.get(character));
+        if (hasUnassigned) {
+            throw new Error('Unassigned code point, see https://tools.ietf.org/html/rfc4013#section-2.5');
+        }
+    }
+    const hasBidiRAL = normalized_map.some((character) => bidirectional_r_al.get(character));
+    const hasBidiL = normalized_map.some((character) => bidirectional_l.get(character));
+    if (hasBidiRAL && hasBidiL) {
+        throw new Error('String must not contain RandALCat and LCat at the same time,' +
+            ' see https://tools.ietf.org/html/rfc3454#section-6');
+    }
+    const isFirstBidiRAL = bidirectional_r_al.get(getCodePoint(first(normalized_input)));
+    const isLastBidiRAL = bidirectional_r_al.get(getCodePoint(last(normalized_input)));
+    if (hasBidiRAL && !(isFirstBidiRAL && isLastBidiRAL)) {
+        throw new Error('Bidirectional RandALCat character must be the first and the last' +
+            ' character of the string, see https://tools.ietf.org/html/rfc3454#section-6');
+    }
+    return normalized_input;
+}
+saslprep.saslprep = saslprep;
+saslprep.default = saslprep;
+module.exports = saslprep;
+//# sourceMappingURL=index.js.map
